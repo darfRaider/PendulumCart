17dc7f6cbf383345e533c209d579a1edc2d26188
Samuel <freaky0815@hotmail.com>
2020-07-01 14:05:10 +0200

    added first test

--- cpp/test/main.cpp
+++ cpp/test/main.cpp
@@ -1,14 +1,27 @@
 #include <gtest/gtest.h>
 #include "IntegratorSimpleStep.hpp"
+#include "Pendulum2d.hpp"
 
-TEST(MyFirstTest, LOL){
-  EXPECT_EQ(0, 0);
+TEST(Integration, TestSimpleStepIntegrator){
+  std::vector<Pendulum2d::TInput> input;
+  for(int i = 0; i < 100; i++){
+	input.push_back(0);
+  }
+  Pendulum2d* p = new Pendulum2d(1,2,3,0.05);
+  
+  p->setInputSequence(&input);
+  Pendulum2d::TState x0 = {0,0,0,0};
+  p->setInitialCondition(x0);
+  std::vector<Pendulum2d::TState>* res = new std::vector<Pendulum2d::TState>;
+  Integrator<Pendulum2d::TState, Pendulum2d>* integrator = 
+	new IntegratorSimpleStep<Pendulum2d::TState,Pendulum2d>(p,0.00132535);
+  integrator->integrate(res);
+ 
+  int nMax = res->size(); 
+  for(int i = 0; i < 4; i++){
+	EXPECT_EQ((*res)[nMax-1][i],0);
+  }
 }
-
-TEST(Integrator, checkZeroIntegration){
-  EXPECT_EQ(0, 1);
-}
-
 
 int main(int argc, char* argv[]){
   testing::InitGoogleTest(&argc, argv);
