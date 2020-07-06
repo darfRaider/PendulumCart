6c97939c85af62f5db79dd1ba12ea1ae757c2378
Samuel <freaky0815@hotmail.com>
2020-07-06 17:57:05 +0200

    wip

--- cpp/lib/simulator/include/Simulator.hpp
+++ cpp/lib/simulator/include/Simulator.hpp
@@ -6,14 +6,18 @@ template<typename System, typename TIntegrator>
 class Simulator {
 
  public:
-   Simulator(System* sys);
-   ~Simulator();
   typedef typename System::TState TState;
   typedef typename System::TInput TInput;
- private:
+  typedef typename std::vector<TInput> TInputSequence;
+  
+  Simulator(System* sys);
+  ~Simulator();
+  void setInputSequence(TInputSequence* inputSequence, double Ts);
+
+private:
   const System* sys;
-  TIntegrator* integrator;
-  std::vector<TInput>* inputSequence;
+  TIntegrator* integrator = 0;
+  TInputSequence* inputSequence = 0;
   TState x0;
 };
 
@@ -23,7 +27,11 @@ Simulator<System, Integrator>::Simulator(System* sys) : sys(sys) {
 }
 
 template<class System, class Integrator>
-Simulator<System, Integrator>::~Simulator() {}
+Simulator<System, Integrator>::~Simulator() {
+  if(integrator != 0)
+	delete integrator;
+}
+
 
 
 #endif
